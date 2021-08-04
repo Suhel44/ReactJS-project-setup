a52dc3e7a4d8c85a2a3ea3c0a06fd4f1b1c8d811
Suhel C <suhel@Suhels-MacBook-Air.local>
2021-08-04 20:40:03 +0530

    Counter fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from './rootReducer';
-import logo from './logo.svg';
-// import Counter from './Counter';
-// import { addCount, minusCount } from './counter';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./rootReducer";
+import logo from "./logo.svg";
+import Counter from "./CounterData";
+import { addCount, minusCount } from "./counters";
+import "./App.css";
 
 export const incrementAsync = () => ({
-  type: 'INCREMENT_ASYNC',
+  type: "INCREMENT_ASYNC",
 });
 
 function App() {
-  const dispatch = useDispatch()
-  const { clicks } = useSelector((state: RootState) => state.count)
+  const dispatch = useDispatch();
+  const { clicks } = useSelector((state: RootState) => state.count);
 
-  // const increment = (page: number) => {
-  //   dispatch(addCount(page));
-  // }
+  const increment = (page: number) => {
+    dispatch(addCount(page));
+  };
 
-  // const decrement = (page: number) => {
-  //   dispatch(minusCount(page));
-  // }
-
+  const decrement = (page: number) => {
+    dispatch(minusCount(page));
+  };
 
   return (
     <div className="App">
-
       {/* Route component will comes here */}
 
-
-
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter
           value={clicks}
@@ -38,69 +34,9 @@ function App() {
           onDecrement={() => decrement(1)}
           onIncrementAsync={() => dispatch(incrementAsync())}
         />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          Learn
-          {" "}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          React,
-          </a>
-          {" "}
-          <a
-            className="App-link"
-            href="https://www.typescriptlang.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Typescript,
-          </a>
-          {" "}
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Redux,
-          </a>
-          {" "}
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          React Redux,
-          </a>
-          {" "}
-          <a
-            className="App-link"
-            href="https://www.npmjs.com/package/redux-injectors"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Redux Injectors
-          </a>
-          {" "}
-          and
-          {" "}
-          <a
-            className="App-link"
-            href="https://redux-saga.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Redux Saga
-          </a>
-        </div>
-      </header> */}
+
+        <p>City moves Sales Associate</p>
+      </header>
     </div>
   );
 }
